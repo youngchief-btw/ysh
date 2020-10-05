8a16437b32255d31a057445713750489f086547b
youngchief btw ツ <33383463+youngchief-btw@users.noreply.github.com>
2020-10-05 15:24:02 -0700

    Ensure certain things are available to YoungShell from the OS

--- src/Setup.sh
+++ src/Setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 clear;
+export USER=$(whoami);
+export HOSTNAME=$(hostname);
 export DIRS=$(dirs -c;dirs);
 g++ $(pwd)/src/YoungShell.cpp -o $(pwd)/ys; -Wall -Wextra -Wpedantic;
 chmod 755 YoungShell;
