8df0eb1481ab6abbc96939d62baff7f66bdfb66f
youngchief btw ツ <33383463+youngchief-btw@users.noreply.github.com>
2020-10-05 14:53:49 -0700

    Delete installer as it's not needed

--- src/Install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone https://github.com/youngchief-btw/YoungShell.git;
-cd YoungShell;
-bash src/PortableSetup.sh
-# echo "/bin/ys" >> /etc/shells;
-# chsh -s /bin/ys;
-# exit && exit
