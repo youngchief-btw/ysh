7ba3d2531f46869c1e180c9f1f7b9641cad85dcb
youngchief btw ツ <33383463+youngchief-btw@users.noreply.github.com>
2020-10-08 16:25:00 -0700

    Better UI and more commands!

--- src/YoungShell.cpp
+++ src/YoungShell.cpp
@@ -1,4 +1,3 @@
-#include <bits/stdc++.h>
 #include <iostream>
 #include <string>
 #include <stdio.h>
@@ -7,10 +6,14 @@
 #include <sstream>
 #include <algorithm>
 #include <iterator>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 using namespace std;
 
 // The following are Linux & MacOS ONLY Terminal color codes.
 // To-Do: Work on getting colors done in Windows (see here: https://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c)
+#ifdef __unix__
 #define RESET "\033[0m"
 #define BLACK "\033[30m" /* Black */
 #define RED "\033[31m" /* Red */
@@ -28,6 +31,9 @@ using namespace std;
 #define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
 #define BOLDCYAN "\033[1m\033[36m" /* Bold Cyan */
 #define BOLDWHITE "\033[1m\033[37m" /* Bold White */
+#define UNDERLINE "\033[4m" /* Underline */
+#define UNDERLINE_OFF "\033[0m" /* Turn off underlining */
+#endif
 
 int recieveInput() {
   string input = WHITE;
@@ -38,7 +44,7 @@ int recieveInput() {
   cin >> input;
 
   if (input == "help") {
-    cout << "Commands: \n- help | Run the YoungShell help command.\n- exit | Exit out of YoungShell (and also terminal if this is your default shell)\n- reload | Reload everything!\n- clear | Clear the shell.\n- shn | Set HostName (requires elevated permissions)\n- gab-do | 'Globally Avaliable Binary Do' - Create a link from here to /bin/ys while keeping all functionally including updates. (requires elevated permissions)\n- gab-undo | 'Globally Avaliable Binary Undo' - Remove the globaly avaliable binary (while still keeping YoungShell installed) (requires elevated permissions)\n- update | Update YoungShell and optionally the host computer (could need elevated permissions)\n";
+    cout << BOLDMAGENTA << "Commands: \n- help | Run the YoungShell help command.\n- exit | Exit out of YoungShell (and also terminal if this is your default shell)\n- reload | Reload everything!\n- clear | Clear the shell.\n- shn | Set HostName (requires elevated permissions)\n- gab | 'Globally Avaliable Binary' (requires elevated permissions)\n- update | Update YoungShell and optionally the host computer (could need elevated permissions)\n- m3 | 'Manage/Modify My Machine' \n" << RESET;
   } else if (input == "exit") {
     cout << BOLDMAGENTA << "Now exiting YoungShell...\n" << RESET;
     return 0;
@@ -46,8 +52,11 @@ int recieveInput() {
     system("bash src/Setup.sh");
     return 0;
   } else if (input == "clear") {
-    system("clear");
-    cout << "\033[1m\033[36m" << "Welcome to YoungShell!\nYoungShell is open-source!! ( https://github.com/youngchief-btw/YoungShell )\n© 2020 youngchief btw ツ, All rights reserved.\nType `exit` to exit!\nType `help` for help!\n" << "\033[34m" << "-----------------------------------------------------------------------------\n" << "\033[0m";
+		system("clear");
+		cout << BOLDCYAN << "Welcome to YoungShell!\nYoungShell is open-source!! ( https://github.com/youngchief-btw/YoungShell )\n© 2020 youngchief btw ツ, All rights reserved.\nType `exit` to exit!\nType `help` for help!\n-----------------------------------------------------------------------------\n" << "\033[0m";
+		#ifdef _WIN32
+		cout << ">| Sorry about no colored output for Windows!\n>| I'm working on it!\n-----------------------------------------------------------------------------\n";
+		#endif
   } else if (input == "shn") {
     cout << "Enter your new hostname. (must have sudo access)\n$ ";
     cin >> input;
@@ -66,7 +75,7 @@ int recieveInput() {
 	} else if (input == "update") {
 		cout << BOLDYELLOW << "Now updating YoungShell..." << RESET << WHITE << "\n";
 		system("git pull https://github.com/youngchief-btw/YoungShell.git;git fetch https://github.com/youngchief-btw/YoungShell.git");
-		cout << RESET << BOLDYELLOW << "Do you want to install system updates as well? (might require sudo permissions) [y/N]:" << RESET << " ";
+		cout << RESET << BOLDYELLOW << "Do you want to install system & application updates as well? (might require sudo permissions) [y/N]:" << RESET << " ";
 		cin >> input;
 		if (input == "y") {
 			#ifdef _WIN32
@@ -78,29 +87,96 @@ int recieveInput() {
 			#ifdef __MACH__
 				system("softwareupdate -I -a");
 			#endif
-			cout << BOLDYELLOW << "Some updates may require a computer reboot!" << RESET << "\n";
+			cout << BOLDYELLOW << "Some updates may require a computer reboot!\n" << RESET;
 		} else {}
-	} else if (input == "gab-do") {
-		cout << "Now creating a link from here to /bin/ys\nMake sure to have sudo permissions!\n";
+	} else if (input == "gab") {
+		cout << BOLDMAGENTA << "Globally Avaliable Binary (GAB) [do/undo]\n" << RESET;
+		cin >> input;
+		if (input == "do") {
+		cout << BOLDMAGENTA << "Now creating a link from here to /bin/ys\nMake sure to have sudo permissions!\n" << RESET;
 		#ifdef _WIN32
 		system("mklink $(echo %cd%)\ys \Windows\System32\ys");
 		#endif
 		#ifdef __unix__
 		system("ln $(pwd)/ys /bin/ys");
 		#endif
-	} else if (input == "gab-undo") {
-		cout << "Now unlinking the globaly avaliable binary from here\nMake sure to have sudo permissions!\n.";
-		system("rm -rf /bin/ys");
-	} else {
-		cout << "Unknown command! Try it in Bash!\nTo get back to YoungShell, just type `exit`\n";
-    system("echo [bash];bash");
+		} else if (input == "undo") {
+			cout << BOLDMAGENTA << "Now unlinking the globally avaliable binary from here\nMake sure to have sudo permissions!\n" << RESET;
+			system("rm -rf /bin/ys");
+		}
+	} else if (input == "m3") {
+		cout << BOLDMAGENTA << "Welcome to Manage My Machine!!\nValid Options: dns (DNS Settings), msc (Machine Specific Commands)\n" << RESET << "$ ";
+		cin >> input;
+		if (input == "dns") {
+			cout << BOLDMAGENTA << ">| DNS Settings\n" << RESET << "$ ";
+			cin >> input;
+			if (input == "setsrvs") {
+				cout << BOLDMAGENTA << ">>| Setting DNS Servers\n" << RESET << "$ ";
+				cin >> input;
+				#ifdef __MACH__
+				system("networksetup -setdnsservers Wi-Fi" << input);
+				#endif
+				#ifdef linux
+				filestream.open("/etc/resolv.conf");
+				if (filestream.is_open()) {
+					filestream << "nameserver " << input;
+				}
+				filestream.close();
+				#endif
+				#ifdef _WIN32
+				#endif
+				cout << BOLDMAGENTA << "DNS Servers Set!\n" << RESET;
+			}
+		} else if (input == "msc") {
+			cout << BOLDMAGENTA << ">| Machine Specific Commands\n>| Valid Options: windows (Windows), \n" << RESET << "$ ";
+			cin >> input;
+			if (input == "windows") {
+				#ifdef __unix__
+				cout << BOLDMAGENTA << "Sorry! This is only for Windows users and you don't qualify!\n";
+				#endif
+				#ifdef _WIN32
+				cout << BOLDMAGENTA << ">>| Windows\n" << RESET << "$ ";
+				cin >> input;
+				if (input == "awwr") {
+					cout << BOLDMAGENTA << "Have you backed up your computer? [y/N]\n" << RESET << "$ ";
+					cin >> input;
+					if (input == "y") {
+						cout << BOLDMAGENTA << "Now attempting 'Activate Windows' watermark removal...\nIf it doesn't work try the steps manually:\n- https://computergarage.org/how-to-remove-activate-windows-10-watermark-permanently.html\n- https://msguides.com/microsoft-software-products/2-ways-activate-windows-10-free-without-software.html\nBe sure to reboot your computer!\n" << RESET;
+						system("reg add HKEY_CURRENT_USER\Control Panel\Desktop /v PaintDesktopVersion /d 0 /f;bcdedit -set TESTSIGNING OFF");
+						cout << BOLDMAGENTA << "Would you like to reboot now? [y/N]\n" << RESET << "$ ";
+						cin >> input;
+						if (input == "y") {
+							cout << BOLDMAGENTA << "Rebooting in 5 seconds...\n" << RESET << "$ ";
+							chrono::milliseconds timespan(5000);
+							this_thread::sleep_for(timespan);
+							system("shutdown /r");
+						} else {
+							cout << BOLDMAGENTA << "Be sure to reboot later!\n" << RESET;
+						}
+					} else {
+						cout << BOLDMAGENTA << "Go backup your computer!\n" << RESET;
+					}
+					#endif
+				} else if (input == "spk") {
+					cout << BOLDMAGENTA << "What would you like to set your product key to?\n" << RESET;
+					cin >> input;
+					system("slmgr.vbs /ipk " + input);
+					cout << BOLDMAGENTA << "\n" << RESET;
+				}
+			}
+		} else {
+		cout << BOLDMAGENTA << "Unknown command! Try it in Bash!\nTo get back to YoungShell, just type `exit`\n" << RESET;
+    system("bash");
   }
   recieveInput();
 }
 
 int main() {
   system("clear");
-  cout << "\033[1m\033[36m" << "Welcome to YoungShell!\nYoungShell is open-source!! ( https://github.com/youngchief-btw/YoungShell )\n© 2020 youngchief btw ツ, All rights reserved.\nType `exit` to exit!\nType `help` for help!\n" << "\033[34m" << "-----------------------------------------------------------------------------\n" << "\033[0m";
+  cout << BOLDCYAN << "Welcome to YoungShell!\nYoungShell is open-source!! ( https://github.com/youngchief-btw/YoungShell )\n© 2020 youngchief btw ツ, All rights reserved.\nType `exit` to exit!\nType `help` for help!\n-----------------------------------------------------------------------------\n" << "\033[0m";
+	#ifdef _WIN32
+	cout << ">| Sorry about no colored output for Windows!\n>I'm working on it!\n-----------------------------------------------------------------------------\n";
+	#endif
   recieveInput();
   return 0;
 }
