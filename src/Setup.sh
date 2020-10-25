5ae0c6fec79732b67dacdb5e81c664c5742c56a1
youngchief btw ツ <33383463+youngchief-btw@users.noreply.github.com>
2020-10-24 18:36:01 -0700

    Revert last commit

--- src/Setup.sh
+++ src/Setup.sh
@@ -3,7 +3,7 @@ clear;
 export USER=$(whoami);
 export HOSTNAME=$(hostname);
 export DIRS=$(dirs -c;dirs +0 || pwd | sed 's|^$HOME|~|' );
-g++ $(pwd)/src/YoungShell.cpp -o $(pwd)/ys -Wall -Wextra -Wpedantic -std=gnu+11;
+g++ $(pwd)/src/YoungShell.cpp -o $(pwd)/ys -Wall -Wextra -Wpedantic;
 chmod 755 ys;
 ./ys
 # rm -rf ys;
